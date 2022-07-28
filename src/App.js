b04c3be04ba1174a307b4d76729d646ee0efe4b9
Clair-Hawkes <hawkesj7@gmail.com>
2022-07-27 17:53:56 -0700

    Base form in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  // HandleForm
+  //Calls our API!
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form encType="multipart/form-data" action="http://localhost:5001/" className="pb-4" method="POST">
+
+        <label htmlFor="file">File:
+          <input type="file" name="file" id="file" required/>
+        </label>
+        <br/>
+
+
+        <label htmlFor="title">Title:
+          <input type="text" name="title" id="title" required/>
+        </label>
+        <br/>
+
+
+        <label htmlFor="description">Description:
+          <input type="text" name="description" id="description" required/>
+        </label>
+        <br/>
+
+
+        <label htmlFor="price">Price:
+          <input type="text" name="price" id="price" required/>
+        </label>
+        <br/>
+
+        <label htmlFor="zipcode">Zipcode:
+          <input type="text" name="zipcode" id="zipcode" required/>
+        </label>
+        <br/>
+
+        <button className="btn btn-primary btn-lg w-100">
+          Uploads a File!
+        </button>
+      </form>
+
+
     </div>
   );
 }
