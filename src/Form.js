752f793d9ad31b7c70910fd77be12e58f67c7916
Clair-Hawkes <hawkesj7@gmail.com>
2022-07-28 15:24:25 -0700

    Use the FormData Obj to catch formData and File to send via axios POST Request to Flask server

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
 const defaultInitialFormData = {
-  title:"",
+  title: "",
   file: "",
   description: "",
-  price:1,
-  zipcode:"",
-  priority: 1 };
+  price: 1,
+  zipcode: "",
+  priority: 1
+};
 
 /** Form for adding a listing.
  *
@@ -39,11 +40,29 @@ function ListingForm({ initialFormData = defaultInitialFormData, handleSave }) {
     evt.preventDefault();
     // handleSave(formData);
     // setFormData(initialFormData);
-    console.log('form-Data',formData)
+    console.log('form-Data', formData);
     console.log(`Selected file - ${fileInput.current.files[0].name}`);
-    formData.file = fileInput.current.files[0];
-    console.log(formData);
-    handleSave(formData);
+    //TODO: FormData Object has unqiue name:
+    //to remind developer of the existance of the FormData Obj.
+    const form = document.querySelector('#createlisting');
+    console.log(form);
+    // const jsFormDataObj = new FormData(form);
+    const jsFormDataObj = new FormData(form);
+
+
+    // jsFormDataObj.append('file', fileInput.current.files[0]);
+    // jsFormDataObj.append('data', formData);
+
+    // jsFormDataObj.append('test','test');
+
+    for(let item of jsFormDataObj){
+      console.log(item);
+    }
+
+
+    // formData.file = fileInput.current.files[0];
+    console.log('jsFormData = ',jsFormDataObj);
+    handleSave(jsFormDataObj);
 
 
     // alert(
@@ -53,90 +72,90 @@ function ListingForm({ initialFormData = defaultInitialFormData, handleSave }) {
   }
 
   return (
-      <form className="NewTodoForm" encType="multipart/form-data" onSubmit={handleSubmit}>
+    <form className="NewTodoForm" encType="multipart/form-data" onSubmit={handleSubmit} id="createlisting">
 
-        <div className="mb-3">
-          <input
-              id="newListing-title"
-              name="title"
-              className="form-control"
-              placeholder="Title"
-              onChange={handleChange}
-              value={formData.title}
-              aria-label="Title"
-          />
-        </div>
+      <div className="mb-3">
+        <input
+          id="newListing-title"
+          name="title"
+          className="form-control"
+          placeholder="Title"
+          onChange={handleChange}
+          value={formData.title}
+          aria-label="Title"
+        />
+      </div>
 
-        <div className="mb-3">
-          <textarea
-              id="newTodo-description"
-              name="description"
-              className="form-control"
-              placeholder="Description"
-              onChange={handleChange}
-              value={formData.description}
-              aria-label="Description"
-          />
-        </div>
+      <div className="mb-3">
+        <textarea
+          id="newTodo-description"
+          name="description"
+          className="form-control"
+          placeholder="Description"
+          onChange={handleChange}
+          value={formData.description}
+          aria-label="Description"
+        />
+      </div>
 
-        <div className="mb-3">
-          <textarea
-              id="newTodo-price"
-              name="price"
-              className="form-control"
-              placeholder="Price"
-              onChange={handleChange}
-              value={formData.price}
-              aria-label="price"
-          />
-        </div>
+      <div className="mb-3">
+        <textarea
+          id="newTodo-price"
+          name="price"
+          className="form-control"
+          placeholder="Price"
+          onChange={handleChange}
+          value={formData.price}
+          aria-label="price"
+        />
+      </div>
 
-        <div className="mb-3">
-          <textarea
-              id="newTodo-zipcode"
-              name="zipcode"
-              className="form-control"
-              placeholder="Zipcode"
-              onChange={handleChange}
-              value={formData.zipcode}
-              aria-label="zipcode"
-          />
-        </div>
+      <div className="mb-3">
+        <textarea
+          id="newTodo-zipcode"
+          name="zipcode"
+          className="form-control"
+          placeholder="Zipcode"
+          onChange={handleChange}
+          value={formData.zipcode}
+          aria-label="zipcode"
+        />
+      </div>
 
 
-        <div className="mb-3">
-          <div className="w-75 d-flex justify-content-between">
+      <div className="mb-3">
+        <div className="w-75 d-flex justify-content-between">
 
-            <label htmlFor="newTodo-priority"
-                   className="d-inline-flex">Priority:&nbsp;&nbsp;
-            </label>
-            <select id="newTodo-priority"
-                    name="priority"
-                    value={formData.priority}
-                    onChange={handleChange}
-                    className="form-control form-control-sm d-inline-flex"
-            >
-              <option value={1}>1 Mile Away</option>
-              <option value={2}>2 Mile Away</option>
-              <option value={3}>3 Miles Away</option>
-            </select>
-          </div>
-
-          <div className="mb-3">
-          <label htmlFor="file">File:
-          <input type="file" name="file" id="file" ref={fileInput} />
+          <label htmlFor="newTodo-priority"
+            className="d-inline-flex">Priority:&nbsp;&nbsp;
           </label>
-          </div>
-
-          <button className="btn-primary btn btn-sm NewTodoForm-addBtn">
-            Find Listing!
-          </button>
-
+          <select id="newTodo-priority"
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+            className="form-control form-control-sm d-inline-flex"
+          >
+            <option value={1}>1 Mile Away</option>
+            <option value={2}>2 Mile Away</option>
+            <option value={3}>3 Miles Away</option>
+          </select>
         </div>
 
+        <div className="mb-3">
+          <label htmlFor="file">File:
+            <input type="file" name="file" id="file" ref={fileInput} />
+          </label>
+        </div>
+
+        <button className="btn-primary btn btn-sm NewTodoForm-addBtn">
+          Find Listing!
+        </button>
+
+      </div>
 
 
-      </form>
+
+    </form>
   );
 }
 
