6c7a33db6e577d1987f6d8c17c01f327c1b20e6e
anela <anela89@gmail.com>
2022-07-29 10:58:44 -0700

    Edited App to reflect changes to Form and API calls.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import { Axios } from 'axios';
 import axios from "axios";
 import "./Box.css";
 import Form from './Form.js';
@@ -11,10 +10,7 @@ const AWS_URL = process.env.REACT_APP_AWS_BASE_URL;
 
 function App() {
   const [listings, setListings] = useState("");
-  console.log("In App", listings);
-  // let new_listing = "";
-  // HandleForm
-  //Calls our API!
+  // console.log("In App", listings);
 
   function Title({ message="Box!" }) {
     return <div><b className="BoxStyles">{message}</b></div>;
@@ -36,13 +32,11 @@ function App() {
     )
   }
 
-  // TODO: Add a useEffect
   useEffect(function fetchListingsOnMount(){
   async function ShareBnB(){
     const resp = (await axios({ url: BASE_URL,method:"get"})).data;
 
     console.log("resp.data = ",resp);
-    // FIXME:
     setListings(resp.listings)
     return resp;
   }
@@ -50,19 +44,11 @@ function App() {
   },[])
 
 
-  // Need POST axios request
-  // Get file submission to Flask to pass to AWS
-  // takes a file from form (formData)
-  // passed to axios POST request /api/listings
-  // resp.data -> new listing {}
-
-  /** Accepts param file as
-   *    fileInput.current.files[0]
+  /** TODO: update docstring to reflect JS FormData Object usage
+   * Creates new listing. 
   */
   async function createBnB(formData){
     console.log("in createBnB", formData);
-    // Condotional check if formData.file is ""
-    //If empty string remove that key from formData
 
     if (!formData) {delete formData.file}
     const resp = (await axios({
@@ -70,8 +56,6 @@ function App() {
       method:"post",
       data:formData,
     }));
-    console.log(resp.data);
-    // FIXME: ChangeLog: Moving new lissting in front of ...Listings old state
     setListings(listings => [resp.data.listing, ...listings]);
   }
 
@@ -87,59 +71,15 @@ function App() {
       <Title message={"Welcome to ShareBnB!"}></Title>
       <Instruction message={"API INSTRUCTIONS"}></Instruction>
       <Form handleSave={createBnB}></Form>
-      {/* TODO: Map listings */}
-      {/* FIXME: */}
-      {!listings? <p>Loading!</p> : <div>{listings.map(listing => <ListingCard key={listing.title} listing={listing} />)}
+      {!listings
+        ? <p>Loading!</p>
+        : <div>{listings.map(listing =>
+            <ListingCard key={listing.title} listing={listing} />)}
       </div>
       }
-
-
 
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-// Form From
-
-// <form encType="multipart/form-data" action="http://localhost:5001/" className="pb-4" method="POST">
-
-// <label htmlFor="file">File:
-//   <input type="file" name="file" id="file" required/>
-// </label>
-// <br/>
-
-
-// <label htmlFor="title">Title:
-//   <input type="text" name="title" id="title" required/>
-// </label>
-// <br/>
-
-
-// <label htmlFor="description">Description:
-//   <input type="text" name="description" id="description" required/>
-// </label>
-// <br/>
-
-
-// <label htmlFor="price">Price:
-//   <input type="text" name="price" id="price" required/>
-// </label>
-// <br/>
-
-// <label htmlFor="zipcode">Zipcode:
-//   <input type="text" name="zipcode" id="zipcode" required/>
-// </label>
-// <br/>
-
-// <button className="btn btn-primary btn-lg w-100">
-//   Uploads a File!
-// </button>
-// </form>
